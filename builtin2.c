e0cae34277e0fcf5ead15f34c47d01e443f06e1a
Adrien Moreau <harutomicmic@hotmail.fr>
2020-01-31 17:49:14 +0100

    all builtin done

--- builtin2.c
+++ builtin2.c
@@ -10,7 +10,7 @@
 #include <unistd.h>
 #include "my.h"
 
-int msetenv(char **splitted, char **envp[], char *li)
+int msetenv(char **splitted, char **envp[], char **arg)
 {
     int i = 0;
     int j = 0;
@@ -18,10 +18,9 @@ int msetenv(char **splitted, char **envp[], char *li)
     int len1 = 0;
     int len2 = 0;
     int pos = -5;
-    char **arg = my_str_to_word_array(li);
     char **rsl = copy(*envp, 2, &pos);
 
-    if (rsl == NULL || pos == -5)
+    if (rsl == NULL || pos == -5 || arg == NULL)
         return 84;
     for (; arg[i] != NULL && my_strcmp(arg[i], splitted[2]) != 0; i++);
     for (; splitted[2][len1] != '\0'; len1 += 1);
@@ -30,11 +29,12 @@ int msetenv(char **splitted, char **envp[], char *li)
     rsl[pos][(len1 + len2 + 1)] = '\0';
     for (; splitted[2][j] != '\0'; rsl[pos][j] = splitted[2][j], j++);
     rsl[pos][j++] = '=';
-    for (; arg[(i + 1)][k] != '\0'; rsl[pos][j++] = arg[(i + 1)][k++]);
+    if (arg[(i + 1)] != NULL)
+        for (; arg[(i + 1)][k] != '\0'; rsl[pos][j++] = arg[(i + 1)][k++]);
     *envp = rsl;
 }
 
-int munsetenv(char **splitted, char *envp[])
+int munsetenv(char **splitted, char **envp[])
 {
     int i = 0;
     int pos = -5;
@@ -43,10 +43,20 @@ int munsetenv(char **splitted, char *envp[])
     if (rsl == NULL || pos == -5)
         return 84;
     for (; rsl[i] != NULL && my_strcmp(rsl[i], splitted[2]) != 0; i++);
+    for (int j = i; rsl[j] != NULL; rsl[i] = rsl[(i + 1)], j++);
+    *envp = rsl;
+    return 0;
 }
 
 int menv(char **splitted, char *envp[])
-{}
+{
+
+    for (int ok = 0; envp[ok] != NULL; ok++)
+        myputstr(envp[ok], 0);
+    return 0;
+}
 
 int mexit(char **splitted, char *envp[])
-{}
+{
+    exit(0);
+}
--- main.c
+++ main.c
@@ -33,23 +33,62 @@ char **copy(char *envp[], int add, int *pos)
     return rsl;
 }
 
+int choice(char **envp[], char **splitted, char **arg)
+{
+    switch (choose(splitted[0]))
+    {
+        case 0:
+            return cd(splitted, *envp);
+        case 1:
+            if (splitted[2][0] == '\0')
+                return menv(splitted, *envp);
+            else
+                return msetenv(splitted, envp, arg);
+        case 2:
+            return munsetenv(splitted, envp);
+        case 3:
+            return menv(splitted, *envp);
+        case 4:
+            return mexit(splitted, *envp);
+        default:
+        break;
+    }
+}
+
 int main(int argc, char *argv[], char *envp[])
 {
-    char *li/*ne*/ = NULL;
-    size_t si/*ze*/;
-    char **split = (char **)malloc(3 * sizeof(char *));
+    char *line = NULL;
+    size_t size;
+    char **arg = NULL;
+    char **splitted = (char **)malloc(3 * sizeof(char *));
 
-    if (myputstr("$>", 1) || (getline(&li, &si, stdin) == -1) || split == NULL)
-        return 84;
-    split = spliter(li, split);
-    choose(split[0]);
-    //cd(split, envp);
-    //msetenv(split, &envp, li);
-/*    printf("%s\n", split[0]);
-    printf("%s\n", split[1]);
-    printf("%s\n", split[2]);*/
-    //printf("iii = %d\n", choose(split[0]));
-//    access(const char *pathname, int mode);
-/*    for (int i = 0; envp[i] != NULL; i++)
-        printf("%s\n", envp[i]);*/
+    while (1) {
+        myputstr("$>", 1);
+        if ((getline(&line, &size, stdin) == -1) || splitted == NULL)
+            return 84;
+        arg = my_str_to_word_array(line);
+        splitted = spliter(line, splitted);
+        choice(&envp, splitted, arg);
+//        test = choose(split[0]);
+/*        if (test == 0)
+            cd(split, envp);
+        else if (test == 1)
+            if (split[2][0] == '\0')
+                menv(split, envp);
+            else
+                msetenv(split, &envp, arg);
+        else if (test == 2)
+            munsetenv(split, &envp);
+        else if (test == 3)
+            menv(split, envp);
+        else if (test == 4)
+            mexit(split, envp);*/
+    /*    printf("%s\n", split[0]);
+        printf("%s\n", split[1]);
+        printf("%s\n", split[2]);*/
+        //printf("iii = %d\n", choose(split[0]));
+    //    access(const char *pathname, int mode);
+    /*    for (int i = 0; envp[i] != NULL; i++)
+            printf("%s\n", envp[i]);*/
+    }
 }
